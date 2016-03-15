62c3db4482612ba8b33d713d3c6366377269a0bf
Martin Kiesel <martin.kiesel@gmail.com>
2016-03-15 22:55:05 +0100

    Initial Commit

---
+++ biometrics-02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  biometrics-02
+//
+//  Created by Martin Kiesel on 15/03/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
