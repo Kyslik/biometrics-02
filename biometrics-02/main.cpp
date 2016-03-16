f0a84bd8b50717a1c9f5c1d6b0bf41f5ab451cce
Martin Kiesel <martin.kiesel@gmail.com>
2016-03-16 10:02:31 +0100

    short data file for input testing, reading from file is done, Xcode project file updated

--- biometrics-02/main.cpp
+++ biometrics-02/main.cpp
@@ -7,9 +7,61 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <utility>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+const char* DATA_FILE = "./data-short.txt";
+
+bool fileExists (const string &fn);
+bool readData(map<char, vector<vector<int>>> &d);
+
+int main(int argc, const char * argv[])
+{
+    map<char, vector<vector<int>>> data;
+    if (!readData(data)) return 1;
+    //data has all the values from file
     return 0;
+}
+
+
+inline bool fileExists (const string& fn)
+{
+    struct stat buff;
+    return (stat (fn.c_str(), &buff) == 0);
+}
+
+bool readData(map<char, vector<vector<int>>> &d)
+{
+    if (!fileExists(DATA_FILE)) return 0;
+
+    ifstream fin(DATA_FILE);
+    string line;
+
+    while (getline(fin, line))
+    {
+        char X = line[0];
+        int i(0), j(0);
+        stringstream stream(line);
+
+        d[X].push_back(vector<int> (16));
+
+        stream.seekg(1);
+        while (stream >> i)
+        {
+
+            d[X][d[X].size() - 1].at(j) = i;
+            if (stream.peek() == ' ')
+                stream.ignore();
+            j++;
+        }
+        
+    }
+    return 1;
 }
