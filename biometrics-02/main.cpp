22576d77000d73300c71130267a50c6ed5d46ace
Martin Kiesel <martin.kiesel@gmail.com>
2016-03-18 00:52:23 +0100

    aliased complicated map<vector<vector> type, aliased map<string, int>; counting functions and size setter

--- biometrics-02/main.cpp
+++ biometrics-02/main.cpp
@@ -16,17 +16,27 @@
 #include <unistd.h>
 
 using namespace std;
+using Data = map<char, vector<vector<int>>>;
+using DataSize = map<string, int>;
 
 const char* DATA_FILE = "./data-short.txt";
 
 bool fileExists (const string &fn);
-bool readData(map<char, vector<vector<int>>> &d);
+bool readData(Data &d);
+
+int divideData(int trainSize, Data &trainData, Data &testData);
+int countData(Data &d);
+
+bool setDataSize(DataSize &s, Data &d, int trainSize);
 
 int main(int argc, const char * argv[])
 {
-    map<char, vector<vector<int>>> data;
+    Data data, testData, trainData;
+    DataSize dataSize;
+
     if (!readData(data)) return 1;
-    //data has all the values from file
+    if (!setDataSize(dataSize, data, 5)) return 1;
+
     return 0;
 }
 
@@ -37,7 +47,28 @@ inline bool fileExists (const string& fn)
     return (stat (fn.c_str(), &buff) == 0);
 }
 
-bool readData(map<char, vector<vector<int>>> &d)
+int divideData(const DataSize dataSize, Data &trainData, Data &testData)
+{
+    return 0;
+}
+
+int countData(Data &d)
+{
+    int i(0);
+    for (const auto &item : d) i += item.second.size();
+    return i;
+}
+
+bool setDataSize(DataSize &s, Data &d, const int trainSize)
+{
+    if (s.find("dataSize") == s.end()) s["dataSize"] = countData(d);
+    if (trainSize >= s["dataSize"]) return 0;
+    s["trainSize"] = trainSize;
+    s["testSize"] = s["dataSize"] - trainSize;
+    return 1;
+}
+
+bool readData(Data &d)
 {
     if (!fileExists(DATA_FILE)) return 0;
 
